67b9e024a42e35eeafef674335f94573bd9f1621
Eyram Quashie <35566981+ekquashie@users.noreply.github.com>
2022-10-30 01:09:14 +0000

    feat: font changed and carousel added

--- src/tools/components/cart-item/cart-item.jsx
+++ src/tools/components/cart-item/cart-item.jsx
@@ -1,85 +1,88 @@
-import { Component } from "react";
+import {Component} from "react";
 
 import s from "./cart-item.module.css";
-import { Query } from "@apollo/client";
-// import { Carousel } from "react-responsive-carousel";
+import { Carousel } from "react-responsive-carousel";
 import Counter from "../counter/counter";
 import {productAttributesRequest} from "../../services/gql-services";
 
 export default class CartItem extends Component {
+  state = {
+    product: {},
+  }
+
+  async componentDidMount() {
+    const product = await productAttributesRequest(this.props.item.name).then((result) => {
+      return result.data.product
+    })
+    this.setState({product: product})
+  }
+
   render() {
-    const { item, currencies } = this.props;
+    const {item, currencies} = this.props;
+    const {product} = this.state;
 
     return (
       <li id={item.id} className={s.item}>
-        {/*<Query query={productAttributesRequest(item.name)}>*/}
-        {/*  {({ loading, error, data }) => {*/}
-        {/*    if (loading) return <p>Loading...</p>;*/}
-        {/*    if (error) return <p>Error : </p>;*/}
-        {/*    const { product } = data;*/}
-        {/*    return (*/}
-        {/*      product && (*/}
-        {/*        <div className={s.miniCard}>*/}
-        {/*          <div className={s.leftSide}>*/}
-        {/*            <p className={s.itemName}>{product.name}</p>*/}
-        {/*            <p className={s.itemPrice}>*/}
-        {/*              {product.prices.map(*/}
-        {/*                (cur) =>*/}
-        {/*                  cur.currency === currencies &&*/}
-        {/*                  `${cur.currency} ${*/}
-        {/*                    Math.round(cur.amount * item.value * 100) / 100*/}
-        {/*                  } `*/}
-        {/*              )}*/}
-        {/*            </p>*/}
-        {/*            <div className={s.attributes}>*/}
-        {/*              {item.attributes.map((attr) => {*/}
-        {/*                return (*/}
-        {/*                  <p*/}
-        {/*                    key={attr}*/}
-        {/*                    style={{ backgroundColor: attr }}*/}
-        {/*                    className={*/}
-        {/*                      attr.includes("#") ? s.coloredLabel : s.itemAttrs*/}
-        {/*                    }*/}
-        {/*                  >*/}
-        {/*                    {attr}*/}
-        {/*                  </p>*/}
-        {/*                );*/}
-        {/*              })}*/}
-        {/*            </div>*/}
-        {/*          </div>*/}
+        {product.name && (
+          <div className={s.miniCard}>
+            <div className={s.leftSide}>
+              <p className={s.itemName}>{product.name}</p>
+              <p className={s.itemPrice}>
+                {product?.prices.map(
+                  (price) =>
+                    price.currency.symbol === currencies &&
+                    `${price.currency.symbol} ${
+                      Math.round(price.amount * item.value * 100) / 100
+                    } `
+                )}
+              </p>
+              <div className={s.attributes}>
+                {item.attributes.map((attr) => {
+                  return (
+                    <p
+                      key={attr}
+                      style={{backgroundColor: attr}}
+                      className={
+                        attr.includes("#") ? s.coloredLabel : s.itemAttrs
+                      }
+                    >
+                      {attr}
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
 
-        {/*          <div className={s.rightSide}>*/}
-        {/*            <Counter pageSize id={item.id} value={item.value} />*/}
-        {/*            /!* <Carousel*/}
-        {/*              className={s.carousel}*/}
-        {/*              showThumbs={false}*/}
-        {/*              showIndicators={false}*/}
-        {/*              width={"141px"}*/}
-        {/*              centerMode={true}*/}
-        {/*              centerSlidePercentage={100}*/}
-        {/*              emulateTouch={true}*/}
-        {/*              swipeable={true}*/}
-        {/*              infiniteLoop={true}*/}
-        {/*              showStatus={false}*/}
-        {/*              useKeyboardArrows={true}*/}
-        {/*            >*/}
-        {/*              {product.gallery.map((image) => {*/}
-        {/*                return (*/}
-        {/*                  <img*/}
-        {/*                    key={image}*/}
-        {/*                    className={s.itemImage}*/}
-        {/*                    src={image}*/}
-        {/*                    alt={product.name}*/}
-        {/*                  />*/}
-        {/*                );*/}
-        {/*              })}*/}
-        {/*            </Carousel> *!/*/}
-        {/*          </div>*/}
-        {/*        </div>*/}
-        {/*      )*/}
-        {/*    );*/}
-        {/*  }}*/}
-        {/*</Query>*/}
+            <div className={s.rightSide}>
+              <Counter pageSize id={item.id} value={item.value}/>
+              <Carousel
+                className={s.carousel}
+                showThumbs={false}
+                showIndicators={false}
+                width={"141px"}
+                centerMode={true}
+                centerSlidePercentage={100}
+                emulateTouch={true}
+                swipeable={true}
+                infiniteLoop={true}
+                showStatus={false}
+                useKeyboardArrows={true}
+              >
+                {product?.gallery?.map((image) => {
+                  return (
+                    <img
+                      key={image}
+                      className={s.itemImage}
+                      src={image}
+                      alt={product?.name}
+                    />
+                  );
+                })}
+              </Carousel>
+            </div>
+          </div>
+        )}
+        );
       </li>
     );
   }
